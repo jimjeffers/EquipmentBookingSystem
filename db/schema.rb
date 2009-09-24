2aca71c00385167a5f1548ac2f61930eb69275a5
Jim Jeffers <jim@sumocreations.com>
2009-09-24 17:34:41 +1000

    Implemented role requirement.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090924072300) do
+ActiveRecord::Schema.define(:version => 20090924073336) do
 
   create_table "bookings", :force => true do |t|
     t.integer  "user_id"
@@ -44,6 +44,18 @@ ActiveRecord::Schema.define(:version => 20090924072300) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "roles", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
+
+  add_index "roles_users", ["role_id"], :name => "index_roles_users_on_role_id"
+  add_index "roles_users", ["user_id"], :name => "index_roles_users_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
