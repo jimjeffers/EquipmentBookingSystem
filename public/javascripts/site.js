02973f3eb9b866d81f7c37fddc72bd7691624789
Jim Jeffers <jim@sumocreations.com>
2009-10-22 11:54:44 +1000

    Implemented CSS 3 progressively enhanced mega dropdown menu for help and guides.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,3 +1,10 @@
+// Unobtrusive Interface Enhancements
+// Written by J. Jeffers
+// ------------------------------------------------------------------
+// Some scripts here have been written specific to this project.
+// Others have been reused from past projects. You can find those
+// here: http://gist.github.com/jimjeffers
+
 $(document).ready(function() {
    // Search Form
    var search_form = $('.search_form input[type="text"]').addClass('fresh');
@@ -8,4 +15,40 @@ $(document).ready(function() {
          current_field.val('');
       }
    });
+   
+   // Easy dropdown navigation with timeouts.
+   $('.sub_navigation').each(function() {
+          var menu = $(this);
+          menu.hide();
+          var parent = $(menu.parent().get(0));
+          var timeout = false; 
+          parent.hover(function(){
+             if(!timeout && !menu.is(':visible')) {
+                timeout = setTimeout(function(){
+                   menu.show();
+                   parent.addClass('active');
+                   timeout = false;
+                }, 350);
+             } else {
+                if(timeout) {
+                   clearTimeout(timeout);
+                }
+                timeout = false;
+             }
+          },
+          function(){
+             if(!timeout && menu.is(':visible')) {
+                 timeout = setTimeout(function(){
+                    menu.hide();
+                    parent.removeClass('active');
+                    timeout = false;
+                 }, 350);
+              } else {
+                 if(timeout) {
+                    clearTimeout(timeout);
+                 }
+                 timeout = false;
+              }
+          });
+      });
 });
