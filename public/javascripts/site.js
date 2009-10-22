140be45e9afd615863ec5ccfe55f88a52410d9f2
Jim Jeffers <jim@sumocreations.com>
2009-10-22 10:46:19 +1000

    Added shopping cart and footer to layout.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
    // Search Form
-   var search_form = $('#primary_search_form input[type="text"]');
+   var search_form = $('.search_form input[type="text"]').addClass('fresh');
    var search_form_default = search_form.val();
    search_form.focus(function() {
-      if(search_form.val() == search_form_default) {
-         search_form.val('');
+      var current_field = $(this).removeClass('fresh');
+      if(current_field.val() == search_form_default) {
+         current_field.val('');
       }
    });
 });
