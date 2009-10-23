0cb4aa866dc7f02592e4a685fc1ea9c02f098cc5
Jim Jeffers <jim@sumocreations.com>
2009-10-23 21:24:51 +1000

    Added help.css

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -4,4 +4,9 @@ class HelpController < ApplicationController
   # Displays the help and guides dashboard.
   def index
   end
+  
+  # Displays a specific solution to a help question.
+  def answer
+  end
+  
 end
